9a75b91f9c33152b31f03402d30aaa5c443e7245
Ricardo <navas.souza@gmail.com>
2020-08-23 16:22:42 -0300

    add pages: List, Product, Producer

---
+++ src/pages/Producer/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Producer: React.FC = () => {
+  return <h1>Producer</h1>;
+}
+
+export default Producer;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as List } from './List';
+export { default as Producer } from './Producer';
+export { default as Product } from './Product';
