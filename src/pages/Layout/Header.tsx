eed2b94455b17c84e9a8f92d318f6f9accf460d5
Ricardo <navas.souza@gmail.com>
2020-08-23 17:10:39 -0300

    add basic layout

---
+++ src/pages/Layout/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HeaderUI, Logo, ListaLink, ListaCount } from './Header.style';
+
+const Header: React.FC = () => {
+  return (
+    <HeaderUI>
+      <Link to="/">
+        <Logo>ceres</Logo>
+      </Link>
+      <Link to="/lista">
+        <ListaLink>
+          lista
+          <ListaCount>3</ListaCount>
+        </ListaLink>
+      </Link>
+    </HeaderUI>
+  );
+};
+
+export default Header;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Home, List, Producer, Product } from 'pages';
+import Layout from 'pages/Layout';
 
 const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/lista" component={List} />
-    <Route path="/produtor/:produtor" component={Producer} />
-    <Route path="/produto/:produto" component={Product} />
-  </Switch>
+  <Layout>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/lista" component={List} />
+      <Route path="/produtor/:produtor" component={Producer} />
+      <Route path="/produto/:produto" component={Product} />
+    </Switch>
+  </Layout>
 );
 
 export default Routes;
