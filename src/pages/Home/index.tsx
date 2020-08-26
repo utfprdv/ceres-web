eda1655d527895afcbd66e90b0697fe7ce230300
Arthur Sosnowski <Metroidling@gmail.com>
2020-08-26 11:58:47 -0300

    added more products for layout test

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,20 +5,30 @@ import { H2 } from './Home.styles';
 
 const products = [
   {
-    image: 'http://lorempixel.com/170/140/food/',
+    image: 'http://lorempixel.com/170/130/food/',
     title: 'Pão caseiro',
     produtores: [{}, {}],
   },
   {
-    image: 'http://lorempixel.com/170/140/food/',
+    image: 'http://lorempixel.com/170/150/food/',
     title: 'Acelga',
     produtores: [{}, {}],
   },
   {
-    image: 'http://lorempixel.com/170/140/food/',
+    image: 'http://lorempixel.com/170/190/food/',
     title: 'Mamão',
     produtores: [{}, {}],
-  }
+  },
+  {
+    image: 'http://lorempixel.com/170/120/food/',
+    title: 'Acelga',
+    produtores: [{}, {}],
+  },
+  {
+    image: 'http://lorempixel.com/170/160/food/',
+    title: 'Mamão',
+    produtores: [{}, {}],
+  },
 ];
 
 const Home: React.FC = () => {
