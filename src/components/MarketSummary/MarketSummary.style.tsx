b9461d1cc4add405fd3a8c51c9ebe0156d2050e2
mhoffnn <matheushofmar@hotmail.com>
2020-09-22 00:12:32 -0300

    visual DropDown construction for city selection

--- src/components/MarketSummary/MarketSummary.style.tsx
+++ src/components/MarketSummary/MarketSummary.style.tsx
@@ -9,14 +9,53 @@ export const MarketDetails = styled.div`
   & > div:last-of-type {
     text-align: right;
   }
-`;
 
-export const MarketTitle = styled.p`
+  .select{
+    padding: 5px;
+    border-style: solid;
+    border-radius: 5px;
+    border-width: 1.5px;
+    border-color: #666666;
+  }
+
+  .selectBox{
+    width: 210px;
+    padding: 5px 20px 10px 10px;
+    position: absolute;
+    background-color: #fffcfa;
+    border-radius: 5px;
+  }
+
+  .options{
+    font-weight: 600;
+    font-size: 16px;
+    margin: 5px;
+    margin-bottom: 5px;
+    margin-top: 10px;
+  }
+
+  .separete{
+    opacity: 0.3;
+    margin: -5px 0 0 -15px;
+    padding: 0 0 0 20px;
+    letter-spacing: -2px;
+  }
+  `;
+  
+  export const MarketTitle = styled.p`
   margin: 0;
   font-weight: 600;
-`;
+  width: 200px;
+  
 
-export const MarketSubtitle = styled.p`
+  .icon{
+    margin: 10px 0 0 82px;
+    opacity: 0.3;
+    position: absolute;
+  }
+  `;
+  
+  export const MarketSubtitle = styled.p`
   font-size: 60%;
   opacity: 0.8;
 `;
--- src/components/MarketSummary/index.tsx
+++ src/components/MarketSummary/index.tsx
@@ -1,22 +1,67 @@
 import React from 'react';
+import { ReactComponent as IconBin } from 'images/selection.svg';
 import {
   MarketDetails,
   MarketTitle,
   MarketSubtitle,
 } from './MarketSummary.style';
 
-const MarketSummary: React.FC = () => {
+type Props= {
+  info:{
+    date:{
+      day: number,
+      month: string,
+      year: number,
+    },
+    amount: number,
+    city: {
+      name: string,
+      address: string,
+    }
+    id: number;
+  }[]
+};
+
+const MarketSummary: React.FC<Props> = ({ info }: Props) => {
   return (
-    <MarketDetails>
-      <div>
-        <MarketTitle>122 produtos</MarketTitle>
-        <MarketSubtitle>14 de setembro, 2020</MarketSubtitle>
-      </div>
-      <div>
-        <MarketTitle>Dois Vizinhos</MarketTitle>
-        <MarketSubtitle>Rua Salgado Filho, 171</MarketSubtitle>
-      </div>
-    </MarketDetails>
+    <>
+    {info.map(info => {
+      return(
+        <MarketDetails>
+          <div>
+            <div className='select'>
+              <MarketTitle>
+                  {info.city.name}
+                  <IconBin className='icon'/>
+                </MarketTitle>
+              <MarketSubtitle> {info.city.address} </MarketSubtitle>
+            </div>
+            <div className='selectBox' >
+              <div className='options' >
+                Dois Vizinhos
+              </div>
+              <div className="separete">_____________________________</div>
+              <div className='options' >
+                Pato Branco
+              </div>
+              <div className="separete">_____________________________</div>
+              <div className='options' >
+                Marmeleiro
+              </div>
+              <div className="separete">_____________________________</div>
+              <div className='options' >
+                Francisco Beltrão
+              </div>
+            </div>
+          </div>
+          <div>
+            <MarketTitle>{info.amount} produtos</MarketTitle>
+            <MarketSubtitle>{info.date.day} de {info.date.month}, {info.date.year}</MarketSubtitle>
+          </div>
+        </MarketDetails>
+      )
+    })}
+    </>
   );
 };
 
