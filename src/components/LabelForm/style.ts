b1064f4bdc8ea879250e8bd14cf2cc2c5e08009b
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2021-01-17 15:22:12 -0300

    Componente label com input para o form

---
+++ src/components/LabelForm/style.ts
@@ -0,0 +1,101 @@
+import styled, { css } from 'styled-components';
+
+interface LABELProps {
+  isVisible: boolean;
+}
+
+export const LABEL = styled.label<LABELProps>`
+  > div {
+    margin-top: 15px;
+    box-sizing: border-box;
+    border-radius: 5px;
+    padding-top: 3px;
+    padding-right: 12px;
+    padding-bottom: 5px;
+    padding-left: 12px;
+    border: 2px solid #80808000;
+
+    ${props =>
+    props.isVisible &&
+    css`
+        border: 2px solid #808080;
+      `};
+
+    input {
+      font-family: Montserrat;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+      color: #000000;
+
+      width: 100%;
+      border: none;
+      background-color: #fde8dd;
+      outline: none;
+      height: 20px;
+      margin: 0px auto;
+    }
+  }
+`;
+
+interface LABELHEADERProps {
+  isInputFilled: boolean;
+}
+
+export const LABELHEADER = styled.div<LABELHEADERProps>`
+  padding-top: 7px;
+  display: flex;
+
+  p {
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: normal;
+    text-transform: lowercase;
+  }
+
+  svg {
+    max-width: 24px;
+    padding-bottom: 15px;
+  }
+
+  ${props =>
+    props.isInputFilled
+      ? css`
+          svg {
+            height: auto;
+            width: 16px;
+          }
+        `
+      : css`
+          svg {
+            height: auto;
+            width: 24px;
+          }
+        `};
+
+  svg + p {
+    padding-left: 10px;
+
+    ${props =>
+    props.isInputFilled
+      ? css`
+            font-size: 12px;
+            line-height: 15px;
+            color: #808080;
+          `
+      : css`
+            font-size: 14px;
+            line-height: 17px;
+            color: #000000;
+          `};
+  }
+
+  p + p {
+    color: #808080;
+    font-size: 12px;
+    line-height: 15px;
+    margin-left: auto;
+    margin-right: 0;
+  }
+`;
--- src/pages/MarketRegistration/index.tsx
+++ src/pages/MarketRegistration/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback, createRef, useState } from 'react';
+import React, { useCallback, createRef } from 'react';
+
+import LABELFORM from '../../components/LabelForm';
 
 import Api from '../../services/api';
-import { H1, FORM, LABEL, LABELHEADER, DIV } from './style';
+import { H1, FORM, DIV } from './style';
 import { ReactComponent as Name } from '../../images/name.svg';
 import { ReactComponent as Cep } from '../../images/cep.svg';
 import { ReactComponent as City } from '../../images/city.svg';
@@ -11,67 +13,6 @@ import { ReactComponent as Complement } from '../../images/complement.svg';
 import { ReactComponent as Email } from '../../images/email.svg';
 import { ReactComponent as StartDate } from '../../images/start_date.svg';
 import { ReactComponent as EndDate } from '../../images/end_date.svg';
-
-interface Props {
-  Icon: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & {
-      title?: string | undefined;
-    }
-  >;
-  Title: string;
-  required?: boolean;
-  type?: string;
-}
-
-const LABELFORM: React.FC<Props> = ({ Icon, Title, required, type }: Props) => {
-  const [isVisible, setIsVisible] = useState(false);
-  const [isInputFilled, setIsInputFilled] = useState(false);
-  const inputRef = createRef<HTMLInputElement>();
-
-  return (
-    <LABEL
-      isVisible={isVisible}
-      onClick={() => {
-        // eslint-disable-next-line no-unused-expressions
-        inputRef.current?.click();
-      }}
-    >
-      <div>
-        <LABELHEADER isInputFilled={isInputFilled}>
-          <Icon />
-          <p>{Title}</p>
-          {required ? <p>Obrigatório</p> : <></>}
-        </LABELHEADER>
-        <input
-          type={type}
-          required={required}
-          ref={inputRef}
-          onChange={() => {
-            if (
-              inputRef?.current?.value !== undefined &&
-              inputRef?.current?.value !== ''
-            ) {
-              setIsInputFilled(true);
-            } else {
-              setIsInputFilled(false);
-            }
-          }}
-          onFocus={() => {
-            setIsVisible(true);
-          }}
-          onBlur={() => {
-            setIsVisible(false);
-          }}
-        />
-      </div>
-    </LABEL>
-  );
-};
-
-LABELFORM.defaultProps = {
-  required: false,
-  type: 'text',
-};
 
 const Login: React.FC = () => {
   const formElement = createRef<HTMLFormElement>();
