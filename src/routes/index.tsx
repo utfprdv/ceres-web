6ea7517e03bb7a467da1dd6595823255f50c7897
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2021-03-23 23:17:16 -0300

    Suspense nas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Switch } from 'react-router-dom';
-import {
-  Home,
-  List,
-  Producer,
-  Product,
-  Login,
-  MarketRegistration,
-  Profile,
-  Cart,
-  Card,
-  PhoneConfirmation,
-  Delivery,
-} from 'pages';
 import Layout from 'pages/Layout';
 import { connect } from 'react-redux';
 import { LOAD_MARKET } from 'store/contants';
 import Route from './Route';
+
+const Home = React.lazy(() => import('pages/Home'));
+const List = React.lazy(() => import('pages/List'));
+const Producer = React.lazy(() => import('pages/Producer'));
+const Product = React.lazy(() => import('pages/Product'));
+const Login = React.lazy(() => import('pages/Login'));
+const MarketRegistration = React.lazy(() => import('pages/MarketRegistration'));
+const Profile = React.lazy(() => import('pages/Profile'));
+const Cart = React.lazy(() => import('pages/Cart'));
+const Card = React.lazy(() => import('pages/Card'));
+const PhoneConfirmation = React.lazy(() => import('pages/PhoneConfirmation'));
+const Delivery = React.lazy(() => import('pages/Delivery'));
 
 const Routes: React.FC = ({ loadMakerts }: any) => {
   React.useEffect(() => {
@@ -25,30 +24,32 @@ const Routes: React.FC = ({ loadMakerts }: any) => {
 
   return (
     <Layout>
-      <Switch>
-        <Route
-          path="/registro/feira"
-          exact
-          component={MarketRegistration}
-          isPrivate
-        />
-        <Route path="/perfil" exact component={Profile} isPrivate />
-        <Route
-          path="/confirmacao-telefone"
-          exact
-          component={PhoneConfirmation}
-          isPrivate
-        />
+      <Suspense fallback={<span>Loading...</span>}>
+        <Switch>
+          <Route
+            path="/registro/feira"
+            exact
+            component={MarketRegistration}
+            isPrivate
+          />
+          <Route path="/perfil" exact component={Profile} isPrivate />
+          <Route
+            path="/confirmacao-telefone"
+            exact
+            component={PhoneConfirmation}
+            isPrivate
+          />
 
-        <Route path="/entrega" exact component={Delivery} />
-        <Route path="/carrinho" exact component={Cart} />
-        <Route path="/cartao" exact component={Card} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/" exact component={Home} />
-        <Route path="/lista" component={List} />
-        <Route path="/produtor/:producerID" component={Producer} />
-        <Route path="/produto/:productID" component={Product} />
-      </Switch>
+          <Route path="/entrega" exact component={Delivery} />
+          <Route path="/carrinho" exact component={Cart} />
+          <Route path="/cartao" exact component={Card} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/" exact component={Home} />
+          <Route path="/lista" component={List} />
+          <Route path="/produtor/:producerID" component={Producer} />
+          <Route path="/produto/:productID" component={Product} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
