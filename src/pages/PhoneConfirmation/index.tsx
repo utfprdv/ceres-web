79ae1b4954205f71d7b4d737bf3df5a0506a846d
Ricardo Corrêa de Souza <navas.souza@gmail.com>
2021-03-24 23:08:12 -0300

    comment out yup one validation

--- src/pages/PhoneConfirmation/index.tsx
+++ src/pages/PhoneConfirmation/index.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useCallback, useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import * as yup from 'yup'
-import { pt } from 'yup-locale-pt'
-import 'yup-phone'
+// import * as yup from 'yup'
+// import { pt } from 'yup-locale-pt'
+// import 'yup-phone'
 
 import { FormProvider as Form } from '../../components/Form'
 import firebase, { auth } from '../../utils/firebase'
@@ -13,7 +14,7 @@ import { H1, DIV, DIVFORM } from './style'
 import { ReactComponent as Phone } from '../../images/phone.svg'
 import { ReactComponent as Confirmation } from '../../images/confirmation.svg'
 
-yup.setLocale(pt)
+// yup.setLocale(pt)
 
 const PhoneConfirmation = (): React.ReactElement => {
   const recaptchaRef = useRef<HTMLDivElement>(null)
@@ -23,93 +24,93 @@ const PhoneConfirmation = (): React.ReactElement => {
 
   const handleOnSubmit = useCallback(
     ({ phone, code }: { phone: string; code: string }) => {
-      const appVerifier = new firebase.auth.RecaptchaVerifier(
-        recaptchaRef.current,
-        {
-          size: 'invisible',
-        },
-      )
+      // const appVerifier = new firebase.auth.RecaptchaVerifier(
+      //   recaptchaRef.current,
+      //   {
+      //     size: 'invisible',
+      //   },
+      // )
 
-      const schema = yup
-        .object({
-          phone: yup.string().phone().required(),
-        })
-        .defined()
+      // const schema = yup
+      //   .object({
+      //     phone: yup.string().phone().required(),
+      //   })
+      //   .defined()
 
-      schema
-        .validate({
-          phone: `+55${(phone.match(/\d+/g) || []).join('')}`,
-        })
-        .then(() => {
-          if (!code)
-            auth.currentUser
-              ?.linkWithPhoneNumber(`+55${phone}`, appVerifier)
-              .then(confirmationResult => {
-                appVerifier.clear()
-                setIsPhoneOk(true)
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                window.confirmationResult = confirmationResult
-              })
-              .catch(error => {
-                switch (error.code) {
-                  case 'auth/provider-already-linked':
-                    setPhoneError(
-                      <span>Um telefone já está associado a esta conta</span>,
-                    )
-                    break
-                  case 'auth/captcha-check-failed':
-                    setPhoneError(
-                      <span>Captcha inválido, tente novamente</span>,
-                    )
-                    break
-                  case 'auth/invalid-phone-number':
-                    setPhoneError(<span>Número de telefone inválido</span>)
-                    break
-                  case 'auth/missing-phone-number':
-                    setPhoneError(<span>Não foi informado um telefone</span>)
-                    break
-                  case 'auth/quota-exceeded':
-                    setPhoneError(
-                      <span>
-                        O limite de sms da deste projeto foi atingido, contate
-                        os responsáveis
-                      </span>,
-                    )
-                    break
-                  case 'auth/user-disabled':
-                    setPhoneError(<span>Usuário desabilitado</span>)
-                    break
-                  case 'auth/credential-already-in-use':
-                    setPhoneError(
-                      <span>
-                        Este telefone já está sendo usado, escolha outro
-                      </span>,
-                    )
-                    break
-                  case 'auth/operation-not-allowed':
-                    setPhoneError(<span>Operação não permitida</span>)
-                    break
-                  case 'auth/too-many-requests':
-                    setPhoneError(
-                      <span>
-                        Nós bloqueamos este dispositivo devido a atividade
-                        anormal. Provavelmente muitas tentativas foram feitas,
-                        tente novamente mais tarde.
-                      </span>,
-                    )
-                    break
-                  default:
-                    setPhoneError(<span>{error.message}</span>)
-                    break
-                }
-              })
-        })
-        .catch(() => {
-          setPhoneError(
-            <span>telefone deve ser um número de telefone válido</span>,
-          )
-        })
+      // schema
+      //   .validate({
+      //     phone: `+55${(phone.match(/\d+/g) || []).join('')}`,
+      //   })
+      //   .then(() => {
+      //     if (!code)
+      //       auth.currentUser
+      //         ?.linkWithPhoneNumber(`+55${phone}`, appVerifier)
+      //         .then(confirmationResult => {
+      //           appVerifier.clear()
+      //           setIsPhoneOk(true)
+      //           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //           // @ts-ignore
+      //           window.confirmationResult = confirmationResult
+      //         })
+      //         .catch(error => {
+      //           switch (error.code) {
+      //             case 'auth/provider-already-linked':
+      //               setPhoneError(
+      //                 <span>Um telefone já está associado a esta conta</span>,
+      //               )
+      //               break
+      //             case 'auth/captcha-check-failed':
+      //               setPhoneError(
+      //                 <span>Captcha inválido, tente novamente</span>,
+      //               )
+      //               break
+      //             case 'auth/invalid-phone-number':
+      //               setPhoneError(<span>Número de telefone inválido</span>)
+      //               break
+      //             case 'auth/missing-phone-number':
+      //               setPhoneError(<span>Não foi informado um telefone</span>)
+      //               break
+      //             case 'auth/quota-exceeded':
+      //               setPhoneError(
+      //                 <span>
+      //                   O limite de sms da deste projeto foi atingido, contate
+      //                   os responsáveis
+      //                 </span>,
+      //               )
+      //               break
+      //             case 'auth/user-disabled':
+      //               setPhoneError(<span>Usuário desabilitado</span>)
+      //               break
+      //             case 'auth/credential-already-in-use':
+      //               setPhoneError(
+      //                 <span>
+      //                   Este telefone já está sendo usado, escolha outro
+      //                 </span>,
+      //               )
+      //               break
+      //             case 'auth/operation-not-allowed':
+      //               setPhoneError(<span>Operação não permitida</span>)
+      //               break
+      //             case 'auth/too-many-requests':
+      //               setPhoneError(
+      //                 <span>
+      //                   Nós bloqueamos este dispositivo devido a atividade
+      //                   anormal. Provavelmente muitas tentativas foram feitas,
+      //                   tente novamente mais tarde.
+      //                 </span>,
+      //               )
+      //               break
+      //             default:
+      //               setPhoneError(<span>{error.message}</span>)
+      //               break
+      //           }
+      //         })
+      //   })
+      //   .catch(() => {
+      //     setPhoneError(
+      //       <span>telefone deve ser um número de telefone válido</span>,
+      //     )
+      //   })
 
       if (code)
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
